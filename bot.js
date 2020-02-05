5e4aa9e1bc4bcff2416583042ecd128ea14f7e3e
hipvpfkd <60709881+hipvpfkd@users.noreply.github.com>
2020-02-05 10:11:21 -0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("674665230814543873")
+setInterval(function() {
+channel.send(`hi pro hi pro hi pro hi pro`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
